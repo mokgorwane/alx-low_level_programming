5134bfa68c36a93e6f626614a70622f9859db89a
mokgorwane <wildibist@gmail.com>
2022-09-20 17:06:40 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
-		if (i % 2 == 0)
+		if (str[i] == '\0')
 		{
-			_putchar(str[i]);
+			_putchar('\n');
+			break;
 		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
 }
